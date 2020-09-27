d34519e3af2039a6ab41729c9de7c0954ad9c5ec
조경진 <kj_cho@idealidea.co.kr>
2020-09-27 13:42:18 +0900

    메인액티비티 주석

--- app/src/main/java/kr/co/tjoeun/pizzaorderapp_20200926/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorderapp_20200926/MainActivity.kt
@@ -7,6 +7,8 @@ import kr.co.tjoeun.pizzaorderapp_20200926.adapters.MainViewPagerAdapter
 
 class MainActivity : BaseActivity() {
 
+//    뷰페이저의 Fragment를 뿌려주기 위한 어댑터 (setValues에서 초기화)
+
     lateinit var mViewPagerAdapter: MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +25,13 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+//        위에서 초기화를 미뤄둔 어댑터를 실제로 채워주는 코드
         mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+
+//        메인화면 뷰페이저의 어댑터로, 방금 완성한 어댑터를 연결
         mainViewPager.adapter = mViewPagerAdapter
 
+//        탭레이아웃의 재료로 메인화면 뷰페이저를 연결.
         mainTabLayout.setupWithViewPager(mainViewPager)
 
     }
