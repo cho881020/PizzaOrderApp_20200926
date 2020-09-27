d72c5666f63fb5822710f78c9abc3ad286d0e292
조경진 <kj_cho@idealidea.co.kr>
2020-09-27 11:50:40 +0900

    가게 정보 상세 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/pizzaorderapp_20200926/ViewStoreDetailActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.pizzaorderapp_20200926
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
